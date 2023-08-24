8e1830a3b01f74295a63df860f2c00e0c81d7ee9
Farhan <farhanmirza52@gmail.com>
2023-08-24 21:34:38 +0530

    security package added

--- app.js
+++ app.js
@@ -11,8 +11,29 @@ const productsRouter = require("./routes/products");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+//Extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const { xss } = require("express-xss-sanitizer");
+const expressLimiter = require("express-rate-limit");
+
 //middleware
 app.use(express.json());
+
+// extra packages
+app.set("trust proxy", 1);
+app.use(
+  expressLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    // store: ... , // Use an external store for more precise rate limiting
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
