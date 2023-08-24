3aa7ba97ff36e7bc9b74029cf5809609607c1c54
Farhan <farhanmirza52@gmail.com>
2023-08-24 21:23:40 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send(
+    '<h1> home page </h1><a href="/api/v1/products">products route </a>'
+  );
+});
+
+//routes
+app.use("/api/v1/products", productsRouter);
+
+//middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connect db
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port:${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
